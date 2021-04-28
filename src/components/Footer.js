a1868e20604414ab83f67d9d074585d4a179e941
Chris Follen <christopher.follen@gmail.com>
2021-04-28 12:59:20 -0600

    added about page

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer(props) {
+    return (
+        <div className={props.footerClass}>
+            <p>© Chris Follen 2021  -  All Rights Reserved</p>
+        </div>
+    )
+}
